3b552cefb90b4df03e5ea5dbb5201d7e923b14c2
Bjørnar Alvestad <BjAlvestad@gmail.com>
2019-10-27 10:59:48 +0100

    Adds protable AutoHotkey v1 + bat to start scripts
    The batfile starts the scripts using the portable AutoHotkey program, stored in the same folder.

---
+++ AutoHotkeyScripts/StartAutoHotkeyScripts.bat
@@ -0,0 +1,11 @@
+:: To run this at startup - put a shortcut in 'shell:startup' (Win+R)
+
+@echo off
+title Startup av AutoHotkey scripts
+
+echo starting WinPadX
+start "" ".\[AutoHotKey_v1.1.31.01]\AutoHotkeyU64.exe" ".\WindowPadX\WindowPadX.ahk"
+
+
+
+:: Other programs - AltDrag,  GridMove (MaxTo or StackWM is probably better alternative than GridMove)
