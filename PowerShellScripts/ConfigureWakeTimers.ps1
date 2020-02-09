152c0e9b6bdb6314e20f5875192a904e12d255fe
Bjørnar Alvestad <BjAlvestad@gmail.com>
2020-02-09 21:51:31 +0100

    Powershell:  Win10 sleep/wakeup configuration (Early version /incomplete scripts)

---
+++ PowerShellScripts/ConfigureWakeTimers.ps1
@@ -0,0 +1,22 @@
+﻿# https://www.tenforums.com/tutorials/63070-enable-disable-wake-timers-windows-10-a.html
+# To get list of possible values:    powercfg /query
+
+Write-Host "This script will help to configure wake timers (i.e. if PC is allowed to wake up for updates, etc.)"
+
+Write-Host "*************************************************************"
+Write-Host "****  Configure waketimers when running on DC (battery)  ****"
+Write-Host "*************************************************************"
+$userSelectionForBattery = 1 # TODO get user input [0,2], where 0=Disabled, 1=Enabled(default), 2=ImportantWakeTimersOnly
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d $userSelectionForBattery
+
+
+Write-Host "***************************************************************"
+Write-Host "****  Configure waketimers when running on AC (pluged in)  ****"
+Write-Host "***************************************************************"
+$userSelectionForAC = 1 #TODO get user input [0,2]
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 238c9fa8-0aad-41ed-83f4-97be242c8f20 bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d $userSelectionForAC
+
+
+# From   powercfg /query
+# GUID: 238c9fa8-0aad-41ed-83f4-97be242c8f20  (Sleep)
+# GUID: bd3b718a-0680-4d9d-8ab2-e1d2b4ac806d  (Allow wake timers)
---
+++ PowerShellScripts/SelectWakeFromSleepHardwareDevices.ps1
@@ -0,0 +1,33 @@
+﻿<#$currentExclusions = Get-MpPreference
+
+$paths = $currentExclusions.ExclusionPath
+$processes = $currentExclusions.ExclusionProcess
+
+Write-Host "`nCurrent exclution paths: `n$($currentExclusions.ExclusionPath)"
+Write-Host "`nCurrent exclution processes: `n$($currentExclusions.ExclusionProcess)"
+
+Remove-MpPreference -ExclusionPath $currentExclusions.ExclusionPath
+Remove-MpPreference -ExclusionProcess $currentExclusions.ExclusionProcess
+
+Write-Host "`nNew exclution paths: `n$($currentExclusions.ExclusionPath)"
+Write-Host "`nNew exclution processes: `n$($currentExclusions.ExclusionProcess)"#>
+
+# https://devblogs.microsoft.com/oldnewthing/20080213-00/?p=23473
+
+Write-Host "This script will help to set which devices you want to be able to wake PC from sleep"
+Write-Host "NOTE: No changes will be performed automatically `n"
+
+Write-Host "********************************************************************"
+Write-Host "****  Devices that can be configured to wake the PC from sleep  ****"
+Write-Host "********************************************************************"
+powercfg -devicequery wake_from_any
+
+Write-Host "********************************************************"
+Write-Host "****  Devices are configured to wake PC from sleep  ****"
+Write-Host "********************************************************"
+powercfg -devicequery wake_armed
+
+Write-Host "`n--------------------------------------------------------------------------------"
+Write-Host "To enable waking from a device, use: `t powercfg -deviceenablewake `"deviceName`""
+Write-Host "To disable waking from a device, use: `t powercfg -devicedisablewake `"deviceName`""
+Write-Host "To disable all, you can use: `n`t`t powercfg /devicequery wake_armed | ForEach{ powercfg /devicedisablewake $_ }"
