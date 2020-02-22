22d247119a9720734f610e6446f78eeb48aa6e1d
BjAlvestad <bjalvestad@gmail.com>
2020-02-22 10:17:33 +0100

    AutoHotkey: Created bat file to start WindowSpy Window spy is used to get information on windows for use in AHK scripts. This bat file makes it easy to start WindowSpy from portable AHK.

---
+++ AutoHotkeyScripts/[AutoHotKey_v1.1.31.01]/Run WindowSpy.bat
@@ -0,0 +1,8 @@
+:: WindowSpy is used to find window names for ahk_class (e.g. to be used to exclude certain windows).
+
+@echo off
+title Startup of WindowSpy
+
+echo starting WinPadX
+start "" ".\AutoHotkeyU64.exe" ".\WindowSpy.ahk"
+
